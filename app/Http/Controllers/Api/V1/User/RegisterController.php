dbc802eb16e9df81ad236d56fc04d2302bfcd2b1
Tho Huynh <tho.hd@workhouse.me>
2020-02-24 13:51:00 +0700

    add api docs

--- app/Http/Controllers/Api/V1/User/RegisterController.php
+++ app/Http/Controllers/Api/V1/User/RegisterController.php
@@ -5,9 +5,33 @@ namespace App\Http\Controllers\Api\V1\User;
 use App\Http\Controllers\BaseApiController;
 use App\Services\User\Version1\Actions\RegisterTeacherAction;
 use App\Http\Requests\User\V1\RegisterTeacherRequest;
+use Illuminate\Http\JsonResponse;
 
 class RegisterController extends BaseApiController
 {
+    /**
+     * @OA\Post(
+     *     path="/api/v1/user/register-teacher",
+     *     operationId="addPet",
+     *     description="register-teacher",
+     *     @OA\RequestBody(
+     *         description="Pet to add to the store",
+     *         required=true,
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="pet response"
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="unexpected error"
+     *     )
+     * )
+     *
+     * @param $request RegisterTeacherRequest
+     * @return JsonResponse
+     */
+
     public function registerTeacher(RegisterTeacherRequest $request)
     {
         $param = $request->only([
--- app/Http/Controllers/BaseApiController.php
+++ app/Http/Controllers/BaseApiController.php
@@ -8,11 +8,22 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
 /**
- * Class BaseApiController.
- *
- * @package App\Http\Controllers
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Server api example",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="huynhductho96@gmail.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
  */
+
 class BaseApiController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ResponseTrait;
